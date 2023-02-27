32c1b76c2a0964e441a5de78de84d15523e5fcc2
olzzon <github@olzzon.dk>
2023-02-27 19:09:12 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # casparcg-batcher
 A simple batch utility to run AMCP commands from the shell or a .bat file. (e.g PLAY 1-10 file LOOP)
+
+
+### Run:
+#### Create a JSON file with the following structure in the same folder as the .exe file:
+
+```
+{
+    "host": "192.168.1.67",
+    "port": 5250,
+    "commands": [
+        "PLAY 2-10 AMB",
+        "PLAY 1-12 [html] https://google.com"
+    ]
+}
+```
+
+#### Open cmd and call:
+```
+casparcg-batcher-win-x64.exe configfile.json
+```
+
+
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { createConnection } from "net";
+import fs from "fs";
+
+interface IConfigFile {
+  host: string;
+  port: number;
+  commands: string[];
+}
+
+let configs: IConfigFile = {host: "127.0.0.1", port: 5250, commands: []};
+
+const configFile = process.argv[1];
+console.log("Using config file", configFile);
+try {
+  configs = JSON.parse(fs.readFileSync(configFile, "utf8"));
+  console.log(configs);
+} catch (e) {
+  console.log("Error:", e.stack);
+}
+
+const socket = createConnection(configs.port, configs.host, () => {
+  console.log("Connected to CasparCG");
+});
+socket
+  .on("data", (data) => {
+    console.log("Received data from CasparCG", data.toString());
+  })
+  .on("connect", () => {
+    console.log("Send command to CasparCG");
+    configs.commands.forEach((cmd: string) => {
+      socket.write(
+        cmd +
+          "\r\n",
+        (e) => (e ? console.log("Error", e) : console.log("Sent"))
+      );
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error", err);
+  })
+  .on("close", () => {
+    console.log("Connection closed");
+  });
